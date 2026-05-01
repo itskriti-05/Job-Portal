ece8032d78e1745c1ec922bd4441f5f694fc8811
kriti goyal <goyalkriti262005@gmail.com>
2026-05-01 11:30:55 +0530

    first commit

---
+++ Frontend/job-portal/src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+
+const LandingPage = () => {
+  return (
+    <div className='min-h-screen'>
+        <Header/>
+        <Hero/>
+      
+    </div>
+  )
+}
+
+export default LandingPage
---
+++ Frontend/job-portal/src/pages/LandingPage/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { motion } from "Framer-motion";
+
+
+const Hero = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Hero
